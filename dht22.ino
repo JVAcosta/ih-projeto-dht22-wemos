f9d392b398e4e6b9baaa395366c31d616577ccea
João D'Amorim <joao.damorim@gmail.com>
2018-02-02 12:52:19 -0300

    dht22

---
+++ dht22.ino
@@ -0,0 +1,82 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  
+  modified 8 Sep 2016
+  by Colby Newman
+*/
+
+
+// the setup function runs once when you press reset or power the board
+/* How to use the DHT-22 sensor with Arduino uno
+   Temperature and humidity sensor
+   More info: http://www.ardumotive.com/how-to-use-dht-22-sensor-en.html
+   Dev: Michalis Vasilakis // Date: 1/7/2015 // www.ardumotive.com */
+
+//Libraries
+#include <DHT.h>
+
+
+//Constants
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void setup()
+{
+    Serial.begin(9600);
+  dht.begin();
+
+}
+
+void loop()
+{
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(2000); //Delay 2 sec.
+    if (temp < 22.00){
+     digitalWrite(7, HIGH);
+     digitalWrite(6, LOW);
+     digitalWrite(5, LOW);
+   }
+   else if (temp >= 22.90 and temp <= 23.00){
+     digitalWrite(7, LOW);
+     digitalWrite(6, HIGH);
+     digitalWrite(5, LOW);
+   }
+   else if (temp > 23.00){
+     digitalWrite(7, LOW);
+     digitalWrite(6, LOW);
+     digitalWrite(5, HIGH);
+  }
+   
+}
+
+   
