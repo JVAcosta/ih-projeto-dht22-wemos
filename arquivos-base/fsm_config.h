e1a19b4e8f99424ba245a194451888aa25f9d39e
João D'Amorim <joao.damorim@gmail.com>
2018-02-05 16:31:01 -0300

    Criação da pasta projeto-2va com os códigos corrigidos e organização dos arquivos.

---
+++ arquivos-base/fsm_config.h
@@ -0,0 +1,34 @@
+#define NUM_STATES 5 // Numero de estados da maquina.
+#define NUM_EVENTS 6 // Numero de eventos da maquina.
+
+// definicao dos possiveis eventos gerados pelos estados da FSM
+typedef enum event_ {btn_pressed, time10, wifiIsConneceted, serverIsConneceted, reconnectWifi, reconnectServer} event; // Typedef: criando e definindo um novo tipo. Enum: btn_pressed: valor 0 e repeat: valor 1. (Eventos dos estados da maquina)  
+
+// definicao das funcoes que implementam o comportamento de cada estado
+event connectWifi(void); 
+event connectServer(void);
+event waitEvent(void); // Termino da maquina de estado. Nao vai ser utilizado (maquina em loop).
+event sendTempHumiTime(void);
+event sendTempHumiBtn(void);
+
+// array de ponteiros para as funcoes dos estados
+event (* state_functions[])(void) = {connectWifi, connectServer, waitEvent, sendTempHumiTime, sendTempHumiBtn}; // Estrutura ninja. Ponteiro para funcao. Armazenamento da funcao em endereco de memoria. Array para buscar funcoes na memoria.
+// definicao dos nomes dos estados
+typedef enum state_ {connectWifi, connectServer, waitEvent, sendTempHumiTime, sendTempHumiBtn} state; // off: valor 0, on: valor 1 e end: valor 2.
+
+// estrutura que define as transicoes dos estados
+state state_transitions[NUM_STATES][NUM_EVENTS] = {{connectWifi, connectWifi, connectServer, connectWifi, connectWifi, connectWifi}, // Implementar transicoes entre estados. Matriz Estados X Eventos. 
+                                                   {connectServer, connectServer, connectServer, waitEvent, connectServer, connectServer},//
+                                                   {sendTempHumiBtn, sendTempHumiTime, waitEvent, waitEvent, waitEvent, waitEvent},
+                                                   {sendTempHumiTime, sendTempHumiTime, sendTempHumiTime, sendTempHumiTime, connectWifi, connectServer},
+                                                   {sendTempHumiBtn,sendTempHumiBtn, sendTempHumiBtn, sendTempHumiBtn, connectWifi, connectServer}
+                                                   };
+// definicao dos estados inicial e final
+#define EXIT_STATE connectWifi //trocar caso for ter um estado de saida
+#define ENTRY_STATE connectWifi
+
+// funcao que implementa a transicao de estados.
+state lookup_transitions(state cur_state, event cur_evt) { // Mapeamento da Matriz Estados X Eventos.
+  return state_transitions[cur_state][cur_evt];
+}
+
---
+++ projeto-2va/fsm-debounce.ino
@@ -0,0 +1,141 @@
+
+
+
+
+#include "fsm_config.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "jualabs";
+const char* password = "jualabsufrpe";
+const char* mqtt_server = "things.ubidots.com";
+const int buttonPin = D5;    // definicao do pino utilizado pelo botao
+const int ledPin = D7;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int i = 0;
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+int buttonState = LOW;             // armazena a leitura atual do botao
+int lastButtonState = LOW;         // armazena a leitura anterior do botao
+unsigned long lastDebounceTime = 0;  // armazena a ultima vez que a leitura da entrada variou
+unsigned long debounceDelay = 50;    // tempo utilizado para implementar o debounce
+
+
+// definicao das funcoes relativas a cada estado
+/*event off_state(void) {
+  digitalWrite(ledPin, LOW);
+  if(read_button())
+    return btn_pressed;
+  else
+    return repeat;
+}
+*/
+
+/*event on_state(void) {
+  digitalWrite(ledPin, HIGH);
+  if(read_button())
+    return btn_pressed;
+  else
+    return repeat;
+}
+*/
+
+
+event connectWifi_state(void) {
+    delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  return wifiIsConnected;
+
+}
+event connectServer_state(void) {
+  Serial.print("Attempting MQTT connection...");
+  if (client.connect("ESP8266Client","A1E-Gl69HF0deyOv90xCmk1jffsJ7Ujk4U","")) {
+      Serial.println("connected");
+      client.subscribe("inTopic");
+      return serverIsConnected;
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+      return reconnectServer;
+    }
+
+  
+}
+
+event waitEvent_state(void) {
+  
+}
+event sendTempHumiTime_state(void) {
+  
+}
+event sendTempHumiBtn_state(void) {
+  
+}
+
+
+// variaveis que armazenam estado atual, evento atual e funcao de tratamento do estado atual
+state cur_state = ENTRY_STATE;
+event cur_evt;
+event (* cur_state_function)(void);
+
+// implementacao de funcoes auxiliares
+int read_button() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  lastButtonState = reading;
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  cur_state_function = state_functions[cur_state];
+  cur_evt = (event) cur_state_function();
+  if (EXIT_STATE == cur_state)
+    return;
+  cur_state = lookup_transitions(cur_state, cur_evt);
+}
